f0882cefe0ab59c66c902a3d720be9aa5ff06b00
oliveira <oliveirita01@gmail.com>
2026-03-10 10:33:33 +0100

    Crear footer y acerca de

---
+++ src/components/AcerCa.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Bienvenido</h1>
+    <p>Esta es la página de información sobre nosotros</p>
+</template>
---
+++ src/components/FooTer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="bg-primary text-white py-3">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="footer-links">
+        <router-link class="text-white me-3" to="/">Aviso Legal</router-link>
+        <router-link class="text-white me-3" to="/">Política de Cookies</router-link>
+        <router-link class="text-white" to="/">Contacto</router-link>
+      </div>
+
+      <div class="footer-copy">
+        &copy; {{ new Date().getFullYear() }} Alexandre Oliveira Vilar, Todos los derechos reservados
+      </div>
+
+    </div>
+  </footer>
+</template>
+
+<script setup>
+</script>
+
+<style>
+footer a {
+  text-decoration: none;
+}
+
+footer a:hover {
+  text-decoration: underline;
+}
+</style>
